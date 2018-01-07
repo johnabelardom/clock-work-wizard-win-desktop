8dd24d31dd83a42a70c67ef827d8c17995b0c262
John Abelardo Manangan II <abelardomanangan@gmail.com>
2018-01-07 21:07:10 +0800

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    'Inherits System.Windows.Forms.Form
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MaterialFlatButton1 = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.MaterialRaisedButton1 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
+        Me.MaterialCheckBox1 = New MaterialSkin.Controls.MaterialCheckBox()
+        Me.MaterialDivider1 = New MaterialSkin.Controls.MaterialDivider()
+        Me.MaterialRadioButton1 = New MaterialSkin.Controls.MaterialRadioButton()
+        Me.MaterialTabControl1 = New MaterialSkin.Controls.MaterialTabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.MaterialTabSelector1 = New MaterialSkin.Controls.MaterialTabSelector()
+        Me.MaterialTabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MaterialFlatButton1
+        '
+        Me.MaterialFlatButton1.AutoSize = True
+        Me.MaterialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialFlatButton1.Depth = 0
+        Me.MaterialFlatButton1.Location = New System.Drawing.Point(203, 336)
+        Me.MaterialFlatButton1.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.MaterialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialFlatButton1.Name = "MaterialFlatButton1"
+        Me.MaterialFlatButton1.Primary = False
+        Me.MaterialFlatButton1.Size = New System.Drawing.Size(172, 36)
+        Me.MaterialFlatButton1.TabIndex = 0
+        Me.MaterialFlatButton1.Text = "MaterialFlatButton1"
+        Me.MaterialFlatButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton1
+        '
+        Me.MaterialRaisedButton1.Depth = 0
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(227, 393)
+        Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.Primary = True
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(208, 44)
+        Me.MaterialRaisedButton1.TabIndex = 1
+        Me.MaterialRaisedButton1.Text = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialLabel1
+        '
+        Me.MaterialLabel1.AutoSize = True
+        Me.MaterialLabel1.Depth = 0
+        Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel1.Location = New System.Drawing.Point(159, 265)
+        Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel1.Name = "MaterialLabel1"
+        Me.MaterialLabel1.Size = New System.Drawing.Size(108, 19)
+        Me.MaterialLabel1.TabIndex = 2
+        Me.MaterialLabel1.Text = "MaterialLabel1"
+        '
+        'MaterialCheckBox1
+        '
+        Me.MaterialCheckBox1.AutoSize = True
+        Me.MaterialCheckBox1.Depth = 0
+        Me.MaterialCheckBox1.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.MaterialCheckBox1.Location = New System.Drawing.Point(115, 158)
+        Me.MaterialCheckBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.MaterialCheckBox1.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.MaterialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCheckBox1.Name = "MaterialCheckBox1"
+        Me.MaterialCheckBox1.Ripple = True
+        Me.MaterialCheckBox1.Size = New System.Drawing.Size(150, 30)
+        Me.MaterialCheckBox1.TabIndex = 3
+        Me.MaterialCheckBox1.Text = "MaterialCheckBox1"
+        Me.MaterialCheckBox1.UseVisualStyleBackColor = True
+        '
+        'MaterialDivider1
+        '
+        Me.MaterialDivider1.BackColor = System.Drawing.Color.FromArgb(CType(CType(31, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialDivider1.Depth = 0
+        Me.MaterialDivider1.Location = New System.Drawing.Point(163, 192)
+        Me.MaterialDivider1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialDivider1.Name = "MaterialDivider1"
+        Me.MaterialDivider1.Size = New System.Drawing.Size(75, 23)
+        Me.MaterialDivider1.TabIndex = 4
+        Me.MaterialDivider1.Text = "MaterialDivider1"
+        '
+        'MaterialRadioButton1
+        '
+        Me.MaterialRadioButton1.AutoSize = True
+        Me.MaterialRadioButton1.Depth = 0
+        Me.MaterialRadioButton1.Font = New System.Drawing.Font("Roboto", 10.0!)
+        Me.MaterialRadioButton1.Location = New System.Drawing.Point(304, 216)
+        Me.MaterialRadioButton1.Margin = New System.Windows.Forms.Padding(0)
+        Me.MaterialRadioButton1.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.MaterialRadioButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRadioButton1.Name = "MaterialRadioButton1"
+        Me.MaterialRadioButton1.Ripple = True
+        Me.MaterialRadioButton1.Size = New System.Drawing.Size(163, 30)
+        Me.MaterialRadioButton1.TabIndex = 5
+        Me.MaterialRadioButton1.TabStop = True
+        Me.MaterialRadioButton1.Text = "MaterialRadioButton1"
+        Me.MaterialRadioButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialTabControl1
+        '
+        Me.MaterialTabControl1.Controls.Add(Me.TabPage1)
+        Me.MaterialTabControl1.Controls.Add(Me.TabPage2)
+        Me.MaterialTabControl1.Depth = 0
+        Me.MaterialTabControl1.Location = New System.Drawing.Point(28, 287)
+        Me.MaterialTabControl1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialTabControl1.Name = "MaterialTabControl1"
+        Me.MaterialTabControl1.SelectedIndex = 0
+        Me.MaterialTabControl1.Size = New System.Drawing.Size(200, 100)
+        Me.MaterialTabControl1.TabIndex = 6
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(192, 71)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(192, 71)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'MaterialTabSelector1
+        '
+        Me.MaterialTabSelector1.BaseTabControl = Nothing
+        Me.MaterialTabSelector1.Depth = 0
+        Me.MaterialTabSelector1.Location = New System.Drawing.Point(12, 76)
+        Me.MaterialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialTabSelector1.Name = "MaterialTabSelector1"
+        Me.MaterialTabSelector1.Size = New System.Drawing.Size(75, 23)
+        Me.MaterialTabSelector1.TabIndex = 7
+        Me.MaterialTabSelector1.Text = "MaterialTabSelector1"
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(616, 543)
+        Me.Controls.Add(Me.MaterialTabSelector1)
+        Me.Controls.Add(Me.MaterialTabControl1)
+        Me.Controls.Add(Me.MaterialRadioButton1)
+        Me.Controls.Add(Me.MaterialDivider1)
+        Me.Controls.Add(Me.MaterialCheckBox1)
+        Me.Controls.Add(Me.MaterialLabel1)
+        Me.Controls.Add(Me.MaterialRaisedButton1)
+        Me.Controls.Add(Me.MaterialFlatButton1)
+        Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login - Clock Work Wizard"
+        Me.MaterialTabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MaterialFlatButton1 As MaterialSkin.Controls.MaterialFlatButton
+    Friend WithEvents MaterialRaisedButton1 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents MaterialCheckBox1 As MaterialSkin.Controls.MaterialCheckBox
+    Friend WithEvents MaterialDivider1 As MaterialSkin.Controls.MaterialDivider
+    Friend WithEvents MaterialRadioButton1 As MaterialSkin.Controls.MaterialRadioButton
+    Friend WithEvents MaterialTabControl1 As MaterialSkin.Controls.MaterialTabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents MaterialTabSelector1 As MaterialSkin.Controls.MaterialTabSelector
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports MaterialSkin
+
+Partial Class Login
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+        SkinManager.ColorScheme = New ColorScheme(Primary.Teal800, Primary.Teal900, Primary.Teal500, Accent.LightGreen700, TextShade.WHITE)
+    End Sub
+
+End Class
